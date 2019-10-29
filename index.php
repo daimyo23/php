e101746fee00a41d5c2b7d2742e10c5fd5fc70f4
daimyo23 <mish3009@gmail.com>
2019-10-29 11:13:51 +0200

    antra uzduotys

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
-<?php
-/?>
-
 <html>
     <head>
-        <title>"Aš valdau HTML!"</title>
+        <title>"Aï¿½ valdau HTML!"</title>
     </head>
     <body>
         <h1> "Daniil - HTML assas" </h1>
         <p> Moku kurti statini turini </p>
+        <?php print date ('Y-m-d His');?>
     </body>
 </html>
-
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
