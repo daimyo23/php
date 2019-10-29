bfe44b8da8d70f3785c27d9f4a262088e1f7aebc
daimyo23 <mish3009@gmail.com>
2019-10-29 10:56:15 +0200

    first commit

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+/?>
+
+<html>
+    <head>
+        <title>"Aš valdau HTML!"</title>
+    </head>
+    <body>
+        <h1> "Daniil - HTML assas" </h1>
+        <p> Moku kurti statini turini </p>
+    </body>
+</html>
+
 
 /* 
  * To change this license header, choose License Headers in Project Properties.
