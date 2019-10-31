7c509b038a560a42e5cec77e54c46832a7741fee
daimyo23 <mish3009@gmail.com>
2019-10-31 11:28:12 +0200

    antra uzduotys

--- index.php
+++ index.php
@@ -3,12 +3,11 @@ $sudas1 = (rand(1, 100));
 
 $sudas2 = (rand(101, 200));
 $sudas3 = (rand(201, 300));
-$sudas4 = (rand(301, 400));
 
 $h1 = "Skolos skaciuokle";
-        $h2 = "Su vienu kabanciku grazinsi" ;
-                $h3 = "Jei paemei";
-                $h4 = "Su dviem  kabancikais grazinsi";
+$h2 = "Su vienu kabanciku grazinsi";
+$h3 = "Jei paemei";
+$h4 = "Su dviem  kabancikais grazinsi";
 ?>
 <html>
     <head>
@@ -17,10 +16,10 @@ $h1 = "Skolos skaciuokle";
     </head>
     <body>
         <div class="body1">
-            <h1><?php print $h1;?></h1>
-            <h3><?php print $h3;?> <?php print $sudas1; ?> jewsu</h3>
-            <h2><?php print $h2;?> <?php print $sudas2; ?> </h2>
-            <h4><?php print $h4;?> <?php print $sudas3 ?></h4>
+            <h1><?php print $h1; ?></h1>
+            <h3><?php print $h3; ?> <?php print $sudas1; ?> jewsu</h3>
+            <h2><?php print $h2; ?> <?php print $sudas2; ?> </h2>
+            <h4><?php print $h4; ?> <?php print $sudas3 ?></h4>
 
         </div>
     </body>
