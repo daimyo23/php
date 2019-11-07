1044e4d70895bb92827d082fcc4406348d9709f0
daimyo23 <mish3009@gmail.com>
2019-11-07 12:14:53 +0200

    antra uzduotys

--- index.php
+++ index.php
@@ -4,22 +4,21 @@ ini_set('xdebug.var_display_max_depth', '10');
 ini_set('xdebug.var_display_max_children', '256');
 ini_set('xdebug.var_display_max_data', '1024');
 
-$maistas = [ 'lentyna1' => [
-"kiausiniai" => '3',
-"grietine" => '1',
-"jogurtas" => '2',
-"Zuvis" => '0',
+$pissling = [ 'takelis' => [
+"eile4" => ['eile' => 'pozicijos_4',
+    'kiekis'=> '2'],
+"eile3" => ['eile' => 'pozicijos_3',
+    'kiekis'=> '1'],
+"eile2" => ['eile' => 'pozicijos_2',
+    'kiekis'=> '1'],
+"eile1" => ['eile' => 'pozicijos_1',
+    'kiekis'=> '1'],
 ],
-[  'lentyna2' => [
-   "pienas" => '1',
-  "kefyras" => '0',
-  "degtine" => '2',
-  "kecupas" => '2',
-], 
+[  'takelis2' =>  'apmiztas' 
  ]
 ];
 
-var_dump ($maistas);
+var_dump ($pissling);
 
      
 
